158b734835c4b68203d60aa1dbe5301acd12fe02
nicolo.grilli <nicolo.grilli@bristol.ac.uk>
2026-01-15 17:06:46 +0000

    triangular strain calculation

--- src/cyclic.py
+++ src/cyclic.py
@@ -10,30 +10,41 @@ from experiment_comp import Experiment_Comp
 
 class Cyclic(Experiment_Comp)
 
-    def __init__(self, cycle_period=1.0, strain_rate=0.001):
+    def __init__(self, cycle_period=1.0, strain_rate=0.001, elastic_strain_range=30):
         super().__init__()
         self.cycle_period = cycle_period # full cycle
         self.strain_rate = strain_rate
+        self.elastic_strain_range = elastic_strain_range
         
     # calculate max and min stress in each cycle
     def max_stress(self):
+        self.max_stress_array = np.zeros(shape=(self.number_of_cycles))
+        self.min_stress_array = np.zeros(shape=(self.number_of_cycles))
+        
+        return 1
 
+    # calculate strain with triangular time dependence
     def triangular_strain(self, strain_rate):
-        amplitude = 
-        phase = (t % self.cycle_period) / self.cycle_period
-    strain = np.zeros_like(phase)
+        self.strain = np.zeros(shape=(self.number_of_cycles))
+        amplitude_cumulative = strain_rate * self.cycle_period
+        for i in len(self.sim_x_data):
+            phase = (self.sim_x_data[i] % self.cycle_period) / self.cycle_period
+            if (phase < 0.25):
+                self.strain[i] = amplitude_cumulative * phase
+            elif (phase < 0.75):
+                self.strain[i] = (amplitude_cumulative / 2.0) - amplitude_cumulative * phase
+            else:
+                self.strain[i] = amplitude_cumulative * phase - amplitude_cumulative
+        return 1
 
-    for i, p in enumerate(phase):
-        if p < 0.25:
-            strain[i] = 4 * amplitude * p
-        elif p < 0.75:
-            strain[i] = 2 * amplitude - 4 * amplitude * p
-        else:
-            strain[i] = -4 * amplitude + 4 * amplitude * p
+    # Least squares fit: y = m x + b. Returns m youngs modulus, b intercept
+    def linear_fit(self, x, y):
+        A = np.vstack([x, np.ones_like(x)]).T
+        m, b = np.linalg.lstsq(A, y, rcond=None)[0]
+        return float(m), float(b)
 
-    return strain
-
-    def calc_residual(self): # override
+    # calculate the difference between experiment and simulation: override
+    def calc_residual(self):
         # read experimental file
         exp_data = pd.read_csv(self.experiment_file)
         # name of postprocessor file in MOOSE format
@@ -46,8 +57,14 @@ class Cyclic(Experiment_Comp)
         # interpolate experimental y over simulated x array
         # which is usually longer
         exp_y_interp_onto_sim = np.interp(sim_x_data, exp_x_data, exp_y_data)
+        # store exp and sim data into data structures
+        self.exp_x_data = exp_x_data
+        self.exp_y_data = exp_y_data
+        self.sim_x_data = sim_x_data
+        self.sim_y_data = sim_y_data
+        self.exp_y_interp_onto_sim = exp_y_interp_onto_sim
         # calculate total number of cycles: assume x data is time
-        self.number_of_cycles = sim_x_data.max()
+        self.number_of_cycles = np.floor(sim_x_data.max() / self.cycle_period)
         # calculate max stress (positive/negative)
-        self.maxstress()
+        self.max_stress()
         return 1
